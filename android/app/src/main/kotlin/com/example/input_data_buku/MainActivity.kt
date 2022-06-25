2bf9bc3db6789b9040f66a19cab7c56181e9338d
Syahrulr26 <102226851+Syahrulr26@users.noreply.github.com>
2022-06-26 03:24:19 +0700

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/input_data_buku/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.input_data_buku
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\user\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Semester 6\\Mobile Programing\\Flutter\\input_data_buku" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\user\\flutter"
+  "PROJECT_DIR=D:\\Semester 6\\Mobile Programing\\Flutter\\input_data_buku"
+  "FLUTTER_ROOT=C:\\Users\\user\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Semester 6\\Mobile Programing\\Flutter\\input_data_buku\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Semester 6\\Mobile Programing\\Flutter\\input_data_buku"
+  "FLUTTER_TARGET=D:\\Semester 6\\Mobile Programing\\Flutter\\input_data_buku\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Semester 6\\Mobile Programing\\Flutter\\input_data_buku\\.dart_tool\\package_config.json"
+)
